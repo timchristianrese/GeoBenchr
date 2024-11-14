821151b6e0f64edcdf78227d23892fd5b81a44fd
Tim Dockenfuß <dockenfuss@campus.tu-berlin.de>
2024-11-14 12:00:12 +0100

    added write queries, both single and multi; adjusted threading

--- benchmark/geomesa/geotools/geotools/src/main/java/com/example/Main.java
+++ benchmark/geomesa/geotools/geotools/src/main/java/com/example/Main.java
@@ -1,34 +1,31 @@
 package com.example;
 
 import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 import org.geotools.api.data.DataStore;
 import org.geotools.api.data.DataStoreFinder;
+import org.geotools.api.data.SimpleFeatureSource;
+import org.geotools.api.data.SimpleFeatureStore;
+import org.geotools.api.data.Transaction;
+import org.geotools.api.feature.simple.SimpleFeature;
+import org.geotools.api.feature.simple.SimpleFeatureType;
 import org.geotools.api.filter.Filter;
+import org.geotools.data.DataUtilities;
+import org.geotools.data.DefaultTransaction;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.filter.text.cql2.CQL;
-
 public class Main {
 
-    public static void runThreads(String instanceName, String username, String password, String catalog, String zookeepers,String queryType, Integer threadCount, Integer limit)
-    {
-        // Create threads
-        CountDownLatch startLatch = new CountDownLatch(1); // Latch to control start of all threads
-
-        for (int i = 0; i < threadCount; i++) {
-            Thread thread = new Thread(new Task(i,instanceName, username, password, catalog, zookeepers, queryType, limit, startLatch));
-            thread.start();
-        }
-
-        // Start threads
-        System.out.println("All threads ready. Starting tasks in parallel...");
-
-        startLatch.countDown(); // Release all threads to start simultaneously
-        // Wait for threads to finish
-
-    }    //
     public static void main(String[] args) {
         if(args.length < 8){
             System.out.println("Please provide all the required arguments, in the following order: instanceName, username, password, catalog, zookeepers, queryType, threadCount, limit");
@@ -43,11 +40,26 @@ public class Main {
         String zookeepers = args[4];
         String queryType = args[5];
         Integer threadCount = Integer.parseInt(args[6]);
-        Integer limit = Integer.parseInt(args[7]);
+
+        CyclicBarrier barrier = new CyclicBarrier(threadCount, () -> {
+            System.out.println("All threads reached the barrier and will proceed together.");
+        });
+
         
+        Integer limit = Integer.parseInt(args[7]);
+        CountDownLatch startLatch = new CountDownLatch(1); // Latch to control start of all threads
+
+        for (int i = 0; i < threadCount; i++) {
+            Thread thread = new Thread(new Task(barrier,i,instanceName, username, password, catalog, zookeepers, queryType, limit, startLatch));
+            thread.start();
+        }
+
+        // Start threads
+        System.out.println("All threads ready. Starting tasks in parallel...");
+
+        startLatch.countDown(); 
         //if arg count is less than 8, then exit
        
-        runThreads(instanceName, username, password, catalog, zookeepers, queryType, threadCount, limit);
     }
 }
 class Task implements Runnable {
@@ -60,9 +72,10 @@ class Task implements Runnable {
     private String zookeepers;
     private String queryType;
     private Integer limit;
+    private CyclicBarrier barrier;
 
 
-    public Task(int threadNumber, String instanceName, String username, String password, String catalog, String zookeepers, String queryType, Integer limit, CountDownLatch startLatch) {
+    public Task(CyclicBarrier barrier, int threadNumber, String instanceName, String username, String password, String catalog, String zookeepers, String queryType, Integer limit, CountDownLatch startLatch) {
         this.threadNumber = threadNumber;
         this.instanceName = instanceName;
         this.username = username;
@@ -72,6 +85,7 @@ class Task implements Runnable {
         this.queryType = queryType;
         this.limit = limit;
         this.startLatch = startLatch;
+        this.barrier = barrier;
     }
 
     @Override
@@ -93,6 +107,19 @@ class Task implements Runnable {
             try{
                 System.out.println("Trying to connect to the datastore");
                 DataStore dataStore = DataStoreFinder.getDataStore(parameters);
+
+                SimpleFeatureStore featureStore;
+
+                //change if you want to write trip data
+                String featureTypeName = "ride_data";
+
+
+                try {
+                    featureStore = (SimpleFeatureStore) dataStore.getFeatureSource(featureTypeName);
+                } catch (ClassCastException e) {
+                    throw new IOException("Feature source does not support writing");
+                }
+
                 System.out.println("Connected to the datastore "+dataStore);
                 for (String str : dataStore.getTypeNames()) {
                     //System.out.println("Feature: "+str);
@@ -107,7 +134,96 @@ class Task implements Runnable {
                     String midTime;
                     Double lat;
                     Double lon;
+                    Double startLon = Math.floor(Math.random() * (13.50421822-13.22833252))+13.22833252;
+                    Double startLat = Math.floor(Math.random() * (52.58061313-52.42922077))+52.42922077;
+                    OffsetDateTime specificTimestamp = OffsetDateTime.of(
+                2023, 7, 1, 12, 0, 0, 0, ZoneOffset.UTC);
+                    // Format the timestamp
+                    String formattedTimestamp = specificTimestamp.format(DateTimeFormatter.ISO_INSTANT);
+
+                    System.out.println(formattedTimestamp);
+                    Integer writes = 20000;
+                    SimpleFeatureType featureType = dataStore.getSchema(featureTypeName);
+                    SimpleFeatureBuilder featureBuilder = new SimpleFeatureBuilder(featureType);
+                    List<SimpleFeature> features = new ArrayList<>();
                     switch(queryType){
+                        //insert just a single point
+                        case "single_insert":
+                            try {
+                                featureType = dataStore.getSchema("ride_data");
+                                featureBuilder = new SimpleFeatureBuilder(featureType);
+                                featureBuilder.set("rider_id", (int) Math.floor(Math.random() * 30));
+                                featureBuilder.set("ride_id", (int) Math.floor(Math.random() * 300));
+                                featureBuilder.set("latitude", startLat);
+                                featureBuilder.set("longitude", startLon);
+                                featureBuilder.set("geom", "POINT("+startLon+" "+startLat+")");
+                                featureBuilder.set("x", "0");
+                                featureBuilder.set("y", "0");
+                                featureBuilder.set("z", "0");
+                                featureBuilder.set("timestamp", formattedTimestamp);
+                                SimpleFeature feature = featureBuilder.buildFeature("ride_data.feature-single-1");
+                                Transaction transaction = new DefaultTransaction("writeTransaction");
+                                barrier.await();
+                                try {
+                                    featureStore.setTransaction(transaction);
+                                    featureStore.addFeatures(DataUtilities.collection(feature));
+                                    transaction.commit();
+                                } catch (Exception e) {
+                                    transaction.rollback();
+                                    e.printStackTrace();
+                                } finally {
+                                    transaction.close();
+                                }
+                            } catch (Exception e) {
+                            }
+                        //insert queries
+                        case "insert": 
+                            try {
+                                
+                                featureType = dataStore.getSchema("ride_data");
+                                //generate random start point within Berlin
+                                for (int i = 0; i < writes; i++) {
+                                    //create a new feature
+                                    
+                                    //replace ride_data with trip_data when wanting to write trip data
+                                    featureBuilder = new SimpleFeatureBuilder(featureType);
+                                    featureBuilder.set("rider_id", (int) Math.floor(Math.random() * 30));
+                                    featureBuilder.set("ride_id", (int) Math.floor(Math.random() * 300));
+                                    featureBuilder.set("latitude", startLat);
+                                    featureBuilder.set("longitude", startLon);
+                                    featureBuilder.set("geom", "POINT("+startLon+" "+startLat+")");
+                                    featureBuilder.set("x", "0");
+                                    featureBuilder.set("y", "0");
+                                    featureBuilder.set("z", "0");
+                                    featureBuilder.set("timestamp", formattedTimestamp);
+                                    
+                                    
+                                    SimpleFeature feature = featureBuilder.buildFeature("ride_data.feature-" + i);
+                                    features.add(feature);
+                                    //increment the startLon and startLat, simulating a diagonal trip
+                                    startLon += 0.0001;
+                                    startLat += 0.0001;
+                                    //increment the timestamp by 1 second
+                                    specificTimestamp = specificTimestamp.plusSeconds(1);
+                                    formattedTimestamp = specificTimestamp.format(DateTimeFormatter.ISO_INSTANT);
+                                }
+                                //set number of writes that should be done
+                                Transaction transaction = new DefaultTransaction("batchWriteTransaction");
+                                barrier.await();
+                                try {
+                                    featureStore.setTransaction(transaction);
+                                    featureStore.addFeatures(DataUtilities.collection(features));
+                                    transaction.commit();
+                                } catch (Exception e) {
+                                    transaction.rollback();
+                                    e.printStackTrace();
+                                } finally {
+                                    transaction.close();
+                                }
+                                //don't create a read filter,    but instead write to the database
+                            }catch(Exception e){
+                                System.out.println("Failed to write to the database");
+                        }
                         case "surrounding":
                             try {
                                 lat = Math.floor(Math.random() * (52.58061313-52.42922077))+52.42922077;
@@ -243,8 +359,25 @@ class Task implements Runnable {
                             }catch(Exception e){
                                 System.out.println("Failed to set filter");
                             }
-
                     }
+                    //run the query
+                    for (String name : dataStore.getTypeNames()) {
+                        System.out.println("Feature: "+name);
+                        SimpleFeatureSource source = dataStore.getFeatureSource(name);
+                        //start the timer
+                        long start = System.currentTimeMillis();
+                        SimpleFeatureCollection allFeatures = source.getFeatures( filter );
+                        //end the timer
+                        long end = System.currentTimeMillis();
+                        long duration = end - start;
+                    }
+                    //run the query
+                    // System.out.println("Filter: "+filter);
+                    // System.out.println("Limit: "+limit);
+                    // System.out.println("Feature: "+str);
+                    // System.out.println("This is the source: "+source);
+                    // System.out.println("This is the filter: "+filter);
+                    // System.out.println("This is the limit: "+limit);
                     
                 }
                 dataStore.dispose();
