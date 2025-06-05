dbcacaefb4d5ea4396a1c911378eda9f2d3c09fa
Tim Dockenfuß <dockenfuss@campus.tu-berlin.de>
2025-06-05 10:32:46 +0200

    converted mobilityDB to spatial SQL; other things

--- data/scripts/transferData.sh
+++ data/scripts/transferData.sh
@@ -6,9 +6,10 @@ echo $IP
 ssh $USER@$IP "mkdir -p /home/tim/data/aviation/; mkdir -p /home/tim/data/cycling; mkdir -p /home/tim/data/ais/; mkdir -p /home/tim/data/pedestrian/; mkdir -p /home/tim/data/movebank/; mkdir -p /home/tim/data/taxi/;"
 
 #aviation
-#scp ../processed/aviation/point*.csv $USER@$IP:/home/tim/data/aviation/
+scp ../processed/aviation/point*.csv $USER@$IP:/home/tim/data/aviation/
 #testwise only copy one
-scp ../processed/aviation/point_NRW_HIGH_01*.csv $USER@$IP:/home/tim/data/aviation/
+#scp ../processed/aviation/point_NRW_HIGH_01*.csv $USER@$IP:/home/tim/data/aviation/
 #resources 
 mkdir -p ../processed/aviation/resources/
 scp ../processed/aviation/resources/* $USER@$IP:/home/tim/data/aviation/resources/
+#insert the data into tables
